150127791e9e706f4562b37be03dc701686485d2
Wajid <wajidmohdali786@gmail.com>
2023-02-09 20:27:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Layout/Header";
+import React,{ useState} from "react";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cartIsShowm,setCartIsShown]=useState(false);
+
+    const showCardHandler=()=>{
+        setCartIsShown(true);
+    };
+    const hideCardHandler=()=>{
+        setCartIsShown(false);
+    };
+    return (
+        <CartProvider>
+            {cartIsShowm && <Cart onClose={hideCardHandler} />}
+            <Header onShowCart={showCardHandler}/> 
+            <main>
+                <Meals />
+            </main>
+        </CartProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary =()=>{
+    return(
+    <section className={classes.summary}>
+        <h2>Delicious Food, at your home</h2>
+        <p>Choose your favourite food item from our broad list of available meals and enjoy a delicious lunch or dinner at home</p>
+        <p>All our meals are cooked with high quality ingredients by our experienced chefs</p>
+    </section>
+    )
+}
+
+export default MealsSummary;
